3d3cfd43e34d9667f34c15a2348f915c8160787c
Nick Perez <nickjperez@gmail.com>
2016-02-20 05:33:55 -0500

    Update riotdispatch.js

--- riotdispatch.js
+++ riotdispatch.js
@@ -3,31 +3,31 @@ if (typeof require !== 'undefined' && typeof riot === 'undefined') {
 }
 
 var RiotDispatch = {
-  _subscribers: [],
-  _debug: false,
-  subscriber: function() {
-    var subscriber = riot.observable({});
-    this.subscribe(subscriber);
-    return subscriber;
-  },
-  subscribe: function(subscriber) {
-    this._subscribers.push(subscriber);
-  },
-  reset: function() {
-    this._subscribers = [];
-  },
-  debug: function(setting) {
-    this._debug = Boolean(setting);
-  },
-  action: function() {
-    var args = [].slice.call(arguments);
-    this._subscribers.forEach(function(el){
-      if (this._debug) {
-        console.log(args, el, '-> global');
-      }
-      el.trigger.apply(el, args);
-    }, this);
-  },
+    _subscribers: [],
+    _debug: false,
+    subscriber: function() {
+        var subscriber = riot.observable({});
+        this.subscribe(subscriber);
+        return subscriber;
+    },
+    subscribe: function(subscriber) {
+        this._subscribers.push(subscriber);
+    },
+    reset: function() {
+        this._subscribers = [];
+    },
+    debug: function(setting) {
+        this._debug = Boolean(setting);
+    },
+    action: function() {
+        var args = [].slice.call(arguments);
+        this._subscribers.forEach(function(el) {
+            if (this._debug) {
+                console.log(args, el, '-> global');
+            }
+            el.trigger.apply(el, args);
+        }, this);
+    },
 };
 
 ['view', 'store', 'api'].forEach(function(nodeType) {
@@ -35,21 +35,21 @@ var RiotDispatch = {
         _subscribers: [],
         subscribe: function(subscriber) {
             RiotDispatch.subscribe(subscriber);
-             this._subscribers.push(subscriber);
+            this._subscribers.push(subscriber);
         },
         subscriber: function() {
-          var subscriber = RiotDispatch.subscriber();
-          this.subscribe(subscriber);
-          return subscriber;
+            var subscriber = RiotDispatch.subscriber();
+            this.subscribe(subscriber);
+            return subscriber;
         },
         action: function() {
-          var args = [].slice.call(arguments);
-          this._subscribers.forEach(function(el){
-            if (RiotDispatch._debug) {
-              console.log(args, el, '-> ' + nodeType);
-            }
-            el.trigger.apply(el, args);
-          }, this);          
+            var args = [].slice.call(arguments);
+            this._subscribers.forEach(function(el) {
+                if (RiotDispatch._debug) {
+                    console.log(args, el, '-> ' + nodeType);
+                }
+                el.trigger.apply(el, args);
+            }, this);
         }
     }
 });
